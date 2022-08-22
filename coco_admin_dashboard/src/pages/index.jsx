9e62d1b2cccf2559c81ea82158c7a68ec939ea17
JayselSilveira <jaysel0013@gmail.com>
2022-08-22 23:25:03 +0530

    coco_admin_dashboard

---
+++ coco_admin_dashboard/src/pages/index.jsx
@@ -0,0 +1,8 @@
+export { default as Overview } from './Overview';
+export { default as Kanban } from './Kanban';
+export { default as Doctors } from './Doctors';
+export { default as Students } from './Students';
+export { default as Calendar } from './Calendar';
+export { default as Area } from './Charts/Area';
+export { default as Line } from './Charts/Line';
+export { default as Stacked } from './Charts/Stacked';
