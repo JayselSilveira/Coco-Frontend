e13aa5704c1fc8780c1fc0786544c19d151cc7a5
Carson <rodriguescarson@hotmail.com>
2022-08-26 14:06:53 +0530

    ch

--- coco_admin_dashboard/src/pages/Report.jsx
+++ coco_admin_dashboard/src/pages/Report.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Report() {
+  useEffect(() => {
+    fetch("https://coco-sih-api-2.herokuapp.com/", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        u_id: id,
+        ans_id: answersid.collection("TestAnswers").doc()._documentPath
+          ._parts[3],
+      }),
+    });
+  }, []);
   return <div>report</div>;
 }
 
