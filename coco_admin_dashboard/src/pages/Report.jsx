1bef0c55d1423adfeca8aa4de0fad7d736c2242b
Carson <rodriguescarson@hotmail.com>
2022-08-26 15:12:26 +0530

    nn

--- coco_admin_dashboard/src/pages/Report.jsx
+++ coco_admin_dashboard/src/pages/Report.jsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 function Report() {
   useEffect(() => {
     fetch("https://coco-sih-api-2.herokuapp.com/", {
-      mode: "no-cors",
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -12,10 +11,18 @@ function Report() {
       body: JSON.stringify({
         u_id: "qnp4TAZYyDZgpCtZHTnoOVM12NT2",
       }),
-    }).then((response) => {
-      console.log(response);
-    });
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   }, []);
+  const summary_text = {
+    all: "Based on our analysis, the system proposes that ",
+    mild: "The child displays no-to mild signs of this class which inplies he/she does NOT suffer from this.",
+    moderate:
+      "The child displays moderate signs of this class which implies that he needs to be monitored and assisted in managing this mental health class using self care means",
+    extreme:
+      "The child displays extreme signs of this class. This is considered alarming. The child needs proper treatment to manage this class of mental health Issue ",
+  };
   return <div>report</div>;
 }
 
